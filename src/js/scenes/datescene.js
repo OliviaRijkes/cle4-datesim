86bfd6c030c42209477a32fe02abc52d6ddcece3
OliviaRijkes <1101595@hr.nl>
2026-06-17 13:53:04 +0200

    textbits werkt met de choices(oud)

--- src/js/scenes/datescene.js
+++ src/js/scenes/datescene.js
@@ -1,14 +1,74 @@
-import { Scene } from "excalibur";
-import {Dateui} from "../ui/dateui.js";
-import {Character} from "../characters/character.js";
+import { Keys, Scene } from "excalibur";
+import { Dateui } from "../ui/dateui.js";
+import { Character } from "../characters/character.js";
 
-export class Datescene extends Scene{
-    onInitialize(engine){
-        console.log('dateScene')
-        this.ui =  new Dateui();
+export class Datescene extends Scene {
+    onInitialize(engine) {
+        console.log('dateScene start')
+        this.dateCharacter = new Character();
+        this.ui = new Dateui();
+        this.add(this.dateCharacter);
         this.add(this.ui);
 
-        this.dateCharacter = new Character();
-        this.add(this.dateCharacter);
+        this.branchName = 'begin'
+        this.branch = this.dateCharacter.dialog.begin
+        this.dialogIndex = 0
     }
+    nextClick() {
+        if (this.branch.length > this.dialogIndex + 1) {
+            this.dialogIndex++
+            this.ui.loadText()
+            console.log(this.branch.length, this.dialogIndex)
+        }
+    }
+    onPreUpdate(engine) {
+        const kb = engine.input.keyboard
+        if (kb.wasPressed(Keys.Space)) {
+            this.nextClick()
+        }
+    }
+    translateTextFunction(text) {
+        //2 functions, bc 2x 2bits
+        for (let i = 0; i < 2; i++) {
+            const functionBit = functionBit[0+i]
+            const valueBit = functionBit[1+i]
+            switch (functionBit) {
+                case 's':
+                    this.changeScene(valueBit)
+                    break;
+                case 'b':
+                    this.changeBranch(valueBit)
+                    break;
+                case 'e':
+                    this.changeEmotion(valueBit)
+                    break;
+                case 'l':
+                    this.changeLove(valueBit)
+                    break;
+                case 'f':
+                    this.changeFriendship(valueBit)
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    changeScene(sceneNumber) {
+
+    }
+    changeBranch(branchNumber) {
+        let name = Object.keys(this.dateCharacter.dialog)[branchNumber]
+        this.branchName = name
+        this.branch = this.dateCharacter.dialog[name]
+    }
+    changeEmotion(emotionNumber) {
+
+    }
+    changeLove(number) {
+
+    }
+    changeFriendship(number) {
+
+    }
+
 }
--- src/js/ui/dateui.js
+++ src/js/ui/dateui.js
@@ -3,9 +3,7 @@ import { Resources } from "../resources.js";
 
 export class Dateui extends ScreenElement {
     onInitialize(engine) {
-        //init vars
-        this.dialogIndex = 0
-        this.branch = this.scene.dateCharacter.dialog.begin
+        
 
         //maintext
         this.mainTextLabel = new Label({
@@ -20,19 +18,18 @@ export class Dateui extends ScreenElement {
         this.addChild(this.mainTextLabel);
 
         //init write: uses branch,dialogIndex
-        console.log(this.branch[this.dialogIndex])
         this.loadText();
         this.loadChoices()
 
         //lovemeter
     }
     loadText() {
-        this.mainTextLabel.text = this.branch[this.dialogIndex]
+        this.mainTextLabel.text = this.scene.branch[this.scene.dialogIndex]
     }
-    //loads the choices in for the amount of choices
     loadChoices() {
         //needs the branch it ended at to load the choices
-        const choices = this.scene.dateCharacter.choices[this.branch]
+        console.log(this.scene.dateCharacter.choices,this.scene.branchName)
+        const choices = this.scene.dateCharacter.choices[this.scene.branchName]
 
         //loops through all choices and makes a label with the 1st text
         for (let i = 0; i < choices.length; i++) {
@@ -52,27 +49,18 @@ export class Dateui extends ScreenElement {
     }
     choiceClick(i) {
         //resets the dialogIndex
-        this.dialogIndex = 0
+        this.scene.dialogIndex = 0
+
+        //change the branch
+        //index of the dialog based of the name
+        const dialogArray = Object.keys(this.scene.dateCharacter.dialog)
+        const branchName = this.scene.dateCharacter.choices[this.scene.branchName][i].branch
+        this.scene.changeBranch(dialogArray.indexOf(branchName))
 
         //set branch & load
-        this.branch = this.scene.dateCharacter.choices[this.branch][i].branch
         this.loadText()
 
         //remove choiceLabels
-    }
 
-    //loads the next text and/or choices
-    nextClick() {
-        if(this.branch.length >this.dialogIndex+1){
-            this.dialogIndex++
-            this.loadText()
-            console.log(this.branch.length, this.dialogIndex)
-        } 
-    }
-    onPreUpdate(engine) {
-        const kb = engine.input.keyboard
-        if (kb.wasPressed(Keys.Space)) {
-            this.nextClick()
-        }
     }
 }
