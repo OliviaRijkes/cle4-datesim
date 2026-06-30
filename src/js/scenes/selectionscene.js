1a3a69fdc3ab3ca9a7306b00281a9c4e113acf4b
OliviaRijkes <1101595@hr.nl>
2026-06-30 11:32:43 +0200

    selectionscene

--- src/js/scenes/selectionscene.js
+++ src/js/scenes/selectionscene.js
@@ -4,13 +4,14 @@ import { Resources } from "../resources";
 export class Selectionscene extends Scene {
     onInitialize(engine) {
         //background
-        const background = new Actor()
-        background.graphics.use(Resources.SelectionScene.toSprite())
-        background.pos= new Vector(640,360)
-        this.add(background)
+        // const background = new Actor()
+        // background.graphics.use(Resources.SelectionScene.toSprite())
+        // background.pos= new Vector(640,360)
+        // this.add(background)
+
         //screen elements
         this.bio = new Actor()
-        this.selectionUI
+        this.selectionUI = new Actor()
         this.confirmButton = new Label({
             text: 'Bevestig date',
             pos: new Vector(200, 600),
@@ -21,8 +22,8 @@ export class Selectionscene extends Scene {
             })
         })
 
-        // this.add(this.bio)
-        // this.add(this.selectionUI)
+        this.add(this.bio)
+        this.add(this.selectionUI)
         this.add(this.confirmButton)
 
         this.loadSelection()
@@ -39,21 +40,6 @@ export class Selectionscene extends Scene {
         const imageSize =100
         const spaceBetween = 120
 
-        for (let i = 0; i < amount; i++) {
-            //spawn an image
-            const image = new Actor()
-            image.graphics.use(Resources.Selection0.toSprite())
-            //get the right position
-            if (amount%2 === 1){
-                image.pos = new Vector(beginXPos+spaceBetween, beginYPos+ spaceBetween*i)
-            } else{
-                image.pos = new Vector(beginXPos, beginYPos+ spaceBetween*i/2)
-            }
-
-            //the image onclick => selectionclick(i)
-            image.on('pointerdown', (i)=>this.selectionClick(i))
-            this.add(image)
-        }
     }
     loadBio() {
         //this.bio = selectedCharacter.bio image
@@ -66,6 +52,8 @@ export class Selectionscene extends Scene {
     }
     confirmClick() {
         //starts the date for pl1
-        //go to the research stuff pl2
+            this.goToScene(`${this.selectedCharacter.name}`)
+
+        //show QR
     }
 }
